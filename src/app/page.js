f7d92098c1b1abdc5ed6f85fe73f0bf2ec04ad85
rishicds <rishi.404@outlook.com>
2024-09-29 00:41:02 +0530

    initial

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1></>
+  );
+}
