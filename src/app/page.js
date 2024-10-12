aab82f74e268e22298cf7aa481ba8c50badb0eca
Janvi <127668005+inkerton@users.noreply.github.com>
2024-10-12 20:30:03 +0000

    Hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Footer from "@/components/Global/Footer";
 import { NavigationMenu } from "@/components/Global/Header";
+import Hero from "@/components/Global/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <NavigationMenu/>
-    <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1>
+    {/* <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1> */}
+    <Hero/>
     <Footer/>
     </>
   );
