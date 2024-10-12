6869b8598701088809980de30aa6d12b06040d75
Janvi <127668005+inkerton@users.noreply.github.com>
2024-10-12 19:57:39 +0000

    header bar using Navigation Menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import { NavigationMenu } from "@/components/Global/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
+    <NavigationMenu/>
     <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1></>
   );
 }
