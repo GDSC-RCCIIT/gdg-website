e57ab7a725ddf2f773b4740d544b563d8fcb25ba
Janvi <127668005+inkerton@users.noreply.github.com>
2024-10-12 20:04:45 +0000

    Footer added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Global/Footer";
 import { NavigationMenu } from "@/components/Global/Header";
 import Image from "next/image";
 
@@ -5,6 +6,8 @@ export default function Home() {
   return (
     <>
     <NavigationMenu/>
-    <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1></>
+    <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1>
+    <Footer/>
+    </>
   );
 }
