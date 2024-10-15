a71f59a3279b52d476303c0dc1d7dbd200357bdf
Janvi <127668005+inkerton@users.noreply.github.com>
2024-10-15 19:32:37 +0000

    signin page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import SignIn from "./(pages)/SignIn";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1></>
+    {/* <h1 className="text-6xl text-center flex-auto py-[20rem]">WELCOME TO THE GDGRCC WEBSITE</h1></> */}
+    <SignIn/>
+    </>
   );
 }
