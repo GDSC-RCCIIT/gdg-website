582d0eb399e1e891ba0ab0ff7cb0710c49d9bad2
Shariq <shariqsd2003@gmail.com>
2024-10-23 00:01:46 +0530

    Enhance TeamsGallery Page

---
+++ src/app/(pages)/TeamsGallery/Teams.js
@@ -0,0 +1,287 @@
+export const teams = [
+    {
+        name: 'Core Team',
+        folder: 'Core Team',
+        members: [
+            {
+                name: 'Anurag Jha ',
+                designation: 'GDSC Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Ayush Pandit',
+                designation: 'Dev Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Atanu Nayak',
+                designation: 'Dev Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Akancha Singh',
+                designation: 'Events Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Jyotishman Sarkar',
+                designation: 'Events Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Upayan De',
+                designation: 'CP/DSA Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Vikash Sangai',
+                designation: 'CP/DSA Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Anupam Ghosh',
+                designation: 'CP/DSA Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Debabrata Mondal',
+                designation: 'Content Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Siddharth Banerjee',
+                designation: 'Content Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Om Mittal',
+                designation: 'ML Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Aryan Paul',
+                designation: 'ML Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Somoprovo Bhattacharjee',
+                designation: 'Design Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Arin Ray',
+                designation: 'Design Lead',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+        ],
+    },
+    {
+        name: 'Dev Team',
+        folder: 'Dev Team',
+        members: [
+            {
+                name: 'Aditya Mayukh Som',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Arka Dutta',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Ayantik Bhaumik',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Bhavesh Agarwal',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Sayan Kumar Sah',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Soumyajit Naskar',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Subhradeep Bera',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Shubham Pandey',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+        ],
+    },
+    {
+        name: 'ML Team',
+        folder: 'ML Team',
+        members: [
+            {
+                name: 'Parthiv Sarkar',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Shakya Majumdar',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Sheetali Maity',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Soumyadipto Pal',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Srijit Das',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Srinjoy Dutta',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Dipan Mondal',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+        ],
+    },
+    {
+        name: 'Events Team',
+        folder: 'Events Team',
+        members: [
+            {
+                name: 'Anurag Dey',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Ayantik Bhaumik',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Debanuj Basak',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Gaurav Bose ',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Piyush Gupta ',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Rajat Subhra Chowdhury',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+        ],
+    },
+    {
+        name: 'Design Team',
+        folder: 'Design Team',
+        members: [
+            {
+                name: 'Aindree Chatterjee',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Anurag Dey',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Apurba Nandi',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Koustav Bhattacharjee',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Prama Ray',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Saumili Roy ',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Sayandeep Pramanik',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Shreyaan Saha',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Soumyadeep Kar',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+        ],
+    },
+    {
+        name: 'Content Team',
+        folder: 'Content Team',
+        members: [
+            {
+                name: 'Abhinandan Barua',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Arya Pandit',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+            {
+                name: 'Spandan Das',
+                imageSrc: '/avatar.png',
+                linkedin: 'https://www.linkedin.com/',
+            },
+        ],
+    },
+];
--- src/app/(pages)/TeamsGallery/page.jsx
+++ src/app/(pages)/TeamsGallery/page.jsx
@@ -1,268 +1,169 @@
 "use client"
-import React, { useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
 import { FaLinkedin } from 'react-icons/fa';
-import { DividerHorizontalIcon } from '@radix-ui/react-icons';
+import { motion } from 'framer-motion';
+import { teams } from './Teams';
 
 const Teams = () => {
-    const teams = [
-        {
-            name: 'Core Team',
-            folder: 'Core Team',
-            members: [
-                {
-                    name: 'Anurag Jha',
-                    designation: 'GDSC Lead',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Ayush Pandit',
-                    designation: 'Dev Lead',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Atanu Nayak',
-                    designation: 'Dev Lead',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Akancha Singh',
-                    designation: 'Events Lead',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-            ],
-        },
-        {
-            name: 'Dev Team',
-            folder: 'Dev Team',
-            members: [
-                {
-                    name: 'Aditya Mayukh Som',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Arka Dutta',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Ayantik Bhaumik',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Bhavesh Agarwal',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-            ],
-        },
-        {
-            name: 'ML Team',
-            folder: 'ML Team',
-            members: [
-                {
-                    name: 'Parthiv Sarkar',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Shakya Majumdar',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Sheetali Maity',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Soumyadipto Pal',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-            ],
-        },
-        {
-            name: 'Events Team',
-            folder: 'Events Team',
-            members: [
-                {
-                    name: 'Anurag Dey',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Ayantik Bhaumik',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Debanuj Basak',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Gaurav Bose',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-            ],
-        },
-        {
-            name: 'Design Team',
-            folder: 'Design Team',
-            members: [
-                {
-                    name: 'Aindree Chatterjee',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Anurag Dey',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Apurba Nandi',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-                {
-                    name: 'Koustav Bhattacharjee',
-                    imageSrc: '/avatar.png',
-                    linkedin: 'www.linkedin.com',
-                },
-            ],
-        },
-    ];
-
     return (
-        <>
-            <section className='bg-white'>
-
-                <div className='bg-white'>
-                    {teams.map((team, teamIndex) => (
-                        <div key={teamIndex} className='team-section'>
-                            <h2 className='text-4xl font-extrabold text-center mb-8 mt-8'>{team.name}</h2>
-                            <div
-                                style={{
-                                    display: 'flex',
-                                    flexWrap: 'wrap',
-                                    justifyContent: 'center',
-                                    gap: '30px',
-                                }}
+        <div>
+            <div className="flex flex-col items-center justify-center py-6">
+                <div className="flex flex-wrap justify-center">
+                    {teams.map((team, index) => (
+                        <div key={index} className="flex flex-col items-center justify-center py-8">
+                            <motion.h1
+                                className="text-5xl font-extrabold bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text"
+                                initial={{ opacity: 0, y: -50 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 0.8 }}
                             >
-                                {team.members.map((member, memberIndex) => (
-                                    <div
-                                        key={memberIndex}
-                                        style={{
-                                            textAlign: 'center',
-                                            flex: '1 1 40%',
-                                            maxWidth: '200px',
-                                            padding: '20px',
-                                            margin: '10px',
-                                            border: '2px solid #ddd',
-                                            borderRadius: '10px',
-                                            backgroundColor: '#f9f9f9',
-                                            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                                        }}
-                                    >
-                                        <Image
-                                            src={member.imageSrc}
-                                            alt={member.name}
-                                            width={100}
-                                            height={100}
-                                            style={{
-                                                borderRadius: '50%',
-                                                marginBottom: '15px',
-                                            }}
-                                        />
-                                        <h3 style={{ fontSize: '18px', marginBottom: '8px' }}>{member.name}</h3>
-                                        <p style={{ color: '#777', marginBottom: '12px' }}>{member.designation}</p>
-                                        <a
-                                            href={member.linkedin}
-                                            target='_blank'
-                                            rel='noopener noreferrer'
-                                            style={{
-                                                color: '#0077b5',
-                                                textDecoration: 'none',
-                                                fontWeight: 'bold',
-                                            }}
-                                        >
-                                            <FaLinkedin /> LinkedIn
-                                        </a>
-                                    </div>
-                                ))}
+                                {team.name}
+                            </motion.h1>
+                            <div className="flex flex-wrap justify-center">
+                                <TeamGroup team={team} />
                             </div>
                         </div>
                     ))}
                 </div>
+            </div>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                <header className="text-center">
+                    <motion.h1
+                        className="text-5xl font-extrabold bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text"
+                        initial={{ opacity: 0, y: -50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8 }}
+                    >
+                        Our Gallery
+                    </motion.h1>
+                    <p className="mx-auto mt-4 max-w-md text-gray-500">
+                        Want to know more about us? Join us today and be a part of these precious events.
+                    </p>
+                </header>
 
-                    <hr className='mt-8'/>
+                <motion.ul
+                    className="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-3"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    variants={{
+                        hidden: { opacity: 0, scale: 0.8 },
+                        visible: { opacity: 1, scale: 1, transition: { staggerChildren: 0.1 } }
+                    }}
+                >
+                    {galleryItems.map((item, index) => (
+                        <GalleryItem key={index} item={item} index={index} />
+                    ))}
+                </motion.ul>
+            </div>
+        </div>
+    );
+};
 
-                <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-                    <header className="text-center">
-                        <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">Our Gallery</h2>
-
-                        <p className="mx-auto mt-4 max-w-md text-gray-500">
-                            Want to know more about US? Then why are you waiting join us today and be a part of these Precious events.
-                        </p>
-                    </header>
-
-                    <ul className="mt-8 grid grid-cols-1 gap-4 lg:grid-cols-3">
-                        <li>
-                            <a href="#" className="group relative block">
-                                <img
-                                    src="https://images.unsplash.com/photo-1455849318743-b2233052fcff?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                    alt=""
-                                    className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-                                />
-
-                                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-                                    <h3 className="text-xl font-medium text-white">IPC Conference</h3>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#" className="group relative block">
-                                <img
-                                    src="https://images.unsplash.com/photo-1523908511403-7fc7b25592f4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                    alt=""
-                                    className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-                                />
-
-                                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-                                    <h3 className="text-xl font-medium text-white">Winter Shorts</h3>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
-                            <a href="#" className="group relative block">
-                                <img
-                                    src="https://images.unsplash.com/photo-1505373877841-8d25f7d46678?q=80&w=812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                    alt=""
-                                    className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90"
-                                />
-
-                                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
-                                    <h3 className="text-xl font-medium text-white">Skinny code Hackathon</h3>
-
-
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </section>
+const TeamGroup = ({ team }) => {
+    return (
+        <>
+            {team.members.map((member, index) => (
+                <TeamMember
+                    key={index}
+                    index={index}
+                    name={member.name}
+                    designation={member.designation}
+                    imageSrc={member.imageSrc}
+                    linkedin={member.linkedin}
+                />
+            ))}
         </>
+    );
+};
+
+const TeamMember = ({ name, designation, imageSrc, linkedin, index }) => {
+    return (
+        <motion.div
+            className="flex flex-col items-center justify-center m-8"
+            initial={{ opacity: 0, y: 50, scale: 0 }}
+            whileInView={{ opacity: 1, y: 0, scale: 1 }}
+            viewport={{ once: true }}
+            transition={{
+                duration: 0.1,
+                type: 'spring',
+                stiffness: 110,
+                delay: 0.1 * index,
+            }}
+        >
+            <div className="w-40 h-40 relative">
+                <Image
+                    src={imageSrc}
+                    alt={name}
+                    layout="fill"
+                    className="rounded-full shadow-2xl object-cover"
+                    style={{ objectFit: 'cover' }}
+                />
+            </div>
+            <motion.p
+                className="text-gray-800 font-semibold dark:text-white"
+                initial={{ opacity: 0, x: -50 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                viewport={{ once: true }}
+                transition={{
+                    duration: 0.3,
+                    type: 'spring',
+                    stiffness: 110,
+                    delay: 0.1 * index,
+                }}
+            >
+                {name}
+            </motion.p>
+            <p className="text-gray-600 dark:text-white">{designation}</p>
+            <a href={linkedin} target="_blank" rel="noopener noreferrer">
+                <FaLinkedin size={25} className="text-black dark:text-white" />
+            </a>
+        </motion.div>
+    );
+};
+
+const galleryItems = [
+    {
+        title: 'IPC Conference',
+        imageUrl:
+            'https://images.unsplash.com/photo-1455849318743-b2233052fcff?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+        title: 'Winter Shorts',
+        imageUrl:
+            'https://images.unsplash.com/photo-1523908511403-7fc7b25592f4?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+        title: 'Skinny code Hackathon',
+        imageUrl:
+            'https://images.unsplash.com/photo-1505373877841-8d25f7d46678?q=80&w=812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+];
+
+const GalleryItem = ({ item, index }) => {
+    return (
+        <motion.li
+            className={`group relative block ${index === 2 ? 'lg:col-span-2 lg:row-span-2 lg:col-start-2' : ''}`}
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{
+                duration: 0.3,
+                type: 'spring',
+                stiffness: 100,
+                delay: 0.1 * index,
+            }}
+        >
+            <img
+                src={item.imageUrl}
+                alt={item.title}
+                className="aspect-square w-full object-cover transition duration-500 group-hover:opacity-90 rounded-xl shadow-lg"
+            />
+            <div className="absolute inset-0 flex flex-col items-start justify-end p-6 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <h3 className="text-2xl font-bold text-white">{item.title}</h3>
+            </div>
+        </motion.li>
     );
 };
 
