745c8dae0bbd97266069027cdb590f05687a7eb3
Ayush Sharma <asharma7588@gmail.com>
2024-11-10 12:34:12 +0530

    Update route.js

--- src/app/api/SendResetPassword/route.js
+++ src/app/api/SendResetPassword/route.js
@@ -12,11 +12,11 @@ export async function POST(req) {
       });
     }
 
-    const transporter = nodemailer.createTransport({
+   const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: "taskmaster991@gmail.com",
-        pass: "kmepakzcabvztekd",
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
